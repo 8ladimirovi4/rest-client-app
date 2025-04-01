e0fe4324659fd5a434e6472aafc38047306337e6
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-04-01 22:34:32 +0300

    fix: remove localStorage hook

--- src/entities/Home/ui/Home.tsx
+++ src/entities/Home/ui/Home.tsx
@@ -4,22 +4,18 @@ import { useSelector } from 'react-redux';
 import { RootState } from 'app/providers/StoreProvider/config/store.ts';
 import Link from 'next/link';
 import { Spinner } from 'shared/index';
-import { useLocalStorage } from 'shared/lib/hooks/useLocalStorage';
 
 export const Home = () => {
   const { isUserLoggedIn, loading } = useSelector(
     (state: RootState) => state.user
   );
-  const [method] = useLocalStorage({
-    key: 'method',
-    defaultValue: 'GET',
-  });
+  const {method} = useSelector((state:RootState) => state.apiRequest)
   if (loading) return <Spinner />;
   if (!isUserLoggedIn) return null;
 
   return (
     <div>
-      <Link href={`/${method}`}>
+      <Link href={`/${method ?? 'GET'}`}>
         <h1>Restful</h1>
       </Link>
       <Link href="/history">
